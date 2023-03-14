54b135884fe751fae523ae90854f5d435135eb06
Georgeb779 <georgebaez123@hotmail.com>
2023-03-14 13:40:20 -0400

    update tailwind colors and ts routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "primary-blue": "#4661e6",
+        "secondary-blue": "#373F68",
+        "light-blue": "#f2f4ff",
+        "snow-white": "#ffffff",
+        "off-white": "#f7f8fd",
+        "deep-orange": "#f49f85",
+        "sky-blue": "#AD1FEA",
+        "steel-blue": "#647196",
+        "bright-blue": "#62bcfa"
+      }
+    }
   },
+
   plugins: []
 };

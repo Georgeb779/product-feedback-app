47c0db5c42ba945ef4dfc477d4215dff3e97eb25
Georgeb779 <georgebaez123@hotmail.com>
2023-03-23 08:49:17 -0400

    add arrow, comment and suggestion_empty icons

---
+++ src/assets/icons/Arrow/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Arrow = () => {
+  return (
+    <svg
+      width={9}
+      height={7}
+      viewBox='0 0 9 7'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path d='M0 6L4 2L8 6' stroke='#4661E6' strokeWidth={2} />
+    </svg>
+  );
+};
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,1 +1,4 @@
-export * from './BurgerMenu';
+export * from "./BurgerMenu";
+export * from "./SuggestionsEmptyIcon";
+export * from "./Arrow";
+export * from "./CommentIcon";

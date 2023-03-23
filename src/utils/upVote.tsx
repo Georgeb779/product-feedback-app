111b93c5f0c8c231d9b0e1fe311f26e81e954516
Georgeb779 <georgebaez123@hotmail.com>
2023-03-23 15:11:53 -0400

    create a fucntion to update upVote

---
+++ src/utils/upVote.tsx
@@ -0,0 +1,37 @@
+import { UpVotes } from "@/services/upvotes";
+import { SetStateAction } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const UpVote = (
+  userId: string,
+  productId: string,
+  title: string,
+  setDisableVote: {
+    (value: SetStateAction<boolean>): void;
+    (arg0: boolean): void;
+  }
+) => {
+  const upVotesService = new UpVotes();
+  const queryClient = useQueryClient();
+
+  return useMutation(() => upVotesService.updateUpVotes(userId, productId), {
+    onMutate: () => {
+      toast("Please wait", {
+        type: "info",
+        toastId: title
+      });
+      setDisableVote(true);
+    },
+    onSuccess: () => {
+      toast.dismiss(title);
+      queryClient.invalidateQueries("Get Suggestions");
+      toast("Voted successfully", {
+        type: "success",
+        toastId: `${title}-success`
+      });
+      setDisableVote(false);
+    }
+  });
+};

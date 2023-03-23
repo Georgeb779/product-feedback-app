9f0086c701a8494ed4a4966388fe3074ca814c08
Georgeb779 <georgebaez123@hotmail.com>
2023-03-23 15:12:14 -0400

    create dropdown component

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Button } from "../Button";
 import style from "./DropDown.module.scss";
 
 export const DropDown = () => {
   return (
     <div className={style.dropdown__container}>
-      <h2>Sort by: <span>Most Upvotes</span> </h2>
-      <button type='button'>+ Add Feedback</button>
+      <h2>
+        Sort by: <span>Most Upvotes</span>{" "}
+      </h2>
+      <Button
+        text={"+ Add Feedback"}
+        onClick={() => {
+          console.log("click");
+        }}
+        type={"primary"}
+      />
     </div>
   );
 };

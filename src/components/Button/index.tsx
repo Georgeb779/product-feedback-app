7907f42a5ec063070b7e40ff4658363ff45b10bd
Georgeb779 <georgebaez123@hotmail.com>
2023-04-01 10:55:18 -0400

    add new type of btn ( edit and go back)

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-export const Button = ({ text, onClick, type }) => {
+export const Button = ({
+  text,
+  onClick,
+  type,
+  arialabel,
+  icon
+}: {
+  text: string;
+  onClick: () => void;
+  type: "primary" | "secondary" | "edit" | "back";
+  arialabel?: string;
+  icon?: React.ReactNode;
+}) => {
   return (
-    <button className={`${style.btn} ${style[type]}`} onClick={() => onClick()}>
+    <button
+      aria-label={arialabel}
+      className={`${style.btn} ${style[type]}`}
+      onClick={() => onClick()}
+    >
+      {icon}
       {text}
     </button>
   );

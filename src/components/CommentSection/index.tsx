d4da197a081f4488724f84bf79543c1b267c0d7d
Georgeb779 <georgebaez123@hotmail.com>
2023-04-01 11:07:04 -0400

    create and style Comment section

---
+++ src/components/CommentSection/index.tsx
@@ -0,0 +1,39 @@
+import { Comments } from "@/services";
+import { countCommentAndReplies } from "@/utils";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
+import { CommentItem } from "../CommentItem";
+import style from "./CommentSection.module.scss";
+
+export const CommentSection = ({ id }: { id: string }) => {
+  const commentService = new Comments();
+
+  const { data, isLoading, error } = useQuery(
+    ["Get Comments By ID", id],
+    async () => await commentService.getCommentByID(id)
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={style.comment_section__container}>
+      <h2>{data && countCommentAndReplies(data)} Comments</h2>
+      <ul>
+        {data &&
+          data.map((comment) => (
+            <li key={comment.id}>
+              <CommentItem
+                image={comment.user.image}
+                name={comment.user.name}
+                username={comment.user.username}
+                content={comment.content}
+                replies={comment.replies}
+              />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
